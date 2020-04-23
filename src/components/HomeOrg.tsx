1431fcd96b3a0dcdebfa16883ceef49b617ca556
Avi <avidrucker@gmail.com>
2020-04-23 10:30:21 -0400

    feat rough dynamic list display

--- src/components/HomeOrg.tsx
+++ src/components/HomeOrg.tsx
@@ -2,12 +2,12 @@ import React, { Fragment } from 'react';
 
 import { HeaderMol } from './HeaderMol';
 import { WelcomeHomeMol } from './WelcomeHomeMol';
-import { SampleHomeMol1Unmarked } from './SampleHomeMol1Unmarked';
-import { SampleHomeMol2Unmarked } from './SampleHomeMol2Unmarked';
-import { SampleHomeMol3Unmarked } from './SampleHomeMol3Unmarked';
+// import { SampleHomeMol1Unmarked } from './SampleHomeMol1Unmarked';
+// import { SampleHomeMol2Unmarked } from './SampleHomeMol2Unmarked';
+// import { SampleHomeMol3Unmarked } from './SampleHomeMol3Unmarked';
 import { SampleHomeMol2Marked } from './SampleHomeMol2Marked';
 import { SampleHomeMol1Marked1Complete } from './SampleHomeMol1Marked1Complete';
-import { LinkBtnMol } from './LinkBtnMol';
+// import { LinkBtnMol } from './LinkBtnMol';
 import { AppWrapper } from './AppWrapper';
 
 
@@ -15,36 +15,17 @@ import { AppWrapper } from './AppWrapper';
 export default function HomeOrg (props: any) {
 	return (
 		<AppWrapper {...props}>
-			<HeaderMol h2={`${props.h2}, Step ${props.step}, Design ${props.designId}`} />
+			<HeaderMol h2={`${props.h2}`} />
 			<section className="flex flex-column justify-between h-100 pa3 tc">
 				{/* CLEAN STATE, NO TODOS */}
 				{
 					props.h2 === "Welcome Home" &&
 					<WelcomeHomeMol demoCurrent="A" />
 				}
-				{/* DEMO FLAG PROPS EXAMPLE */}
-				{/*
-					{!!props.all &&
-							<div>NOT THE DEMO</div>
-						}
-						{!props.all &&
-							<div>THE DEMO</div>
-						}
-				*/}
-				{/* 1 TO-DO */}
-				{
-					props.h2 === "Home 1 Unmarked" &&
-					<SampleHomeMol1Unmarked demoCurrent="C" />
-				}
-				{/* 2 TODOs */}
-				{
-					props.h2 === "Home 2 Unmarked" &&
-					<SampleHomeMol2Unmarked demoCurrent="E" />
-				}
 				{/* 3 TODOs */}
 				{
-					props.h2 === "Home 3 Unmarked" &&
-					<SampleHomeMol3Unmarked demoCurrent="G" />
+					!!props.currentList &&
+					props.currentList
 				}
 				{/* 3 TODOs w/ 2 Marked */}
 				{
@@ -56,13 +37,6 @@ export default function HomeOrg (props: any) {
 					props.h2 === "Home With 1 Done" &&
 					<Fragment>
 						<SampleHomeMol1Marked1Complete demoCurrent="L" />
-						{!props.all &&
-							<LinkBtnMol
-								className="b"
-								text="Return to all screens page"
-								to="/"
-							/>
-						}
 					</Fragment>
 				}
 			</section>
