3fb501bf96a9b02bc482de19a730d60009e0822c
Avi <avidrucker@gmail.com>
2020-04-23 10:31:30 -0400

    update app to use home org to render list

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,25 @@ import React from 'react';
 
 import { mainWebApp } from './model';
 import { IToDoItem } from './core/toDoItem';
+import HomeOrg from './components/HomeOrg';
 
 function App() {
 	let toDoList: IToDoItem[] = [];
 	let lastDone: string = "";
 	[toDoList, lastDone] = mainWebApp(toDoList, lastDone);
 
-	const listItems = <ul>{toDoList.map(x => <li>{x.header}</li>)}</ul>;
+	const listItems = <ul>{
+		toDoList.map(x => 
+					<li className={x.state === 0 ? "unmarked" : "marked"}>
+						{x.header}
+					</li>
+				)
+			}
+		</ul>;
 
   return (
     <section className="debug">
-      <section>{listItems}</section>
+			<HomeOrg h2="Home" currentList={listItems} />
 			<section>Last done is {lastDone}</section>
     </section>
   );
